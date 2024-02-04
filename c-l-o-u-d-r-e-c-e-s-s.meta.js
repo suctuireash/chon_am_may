7ed5324725cef58bf0bf25fd087306909de1f575
suctuireash <158839926+suctuireash@users.noreply.github.com>
2024-02-04 16:31:01 +0000

    init

---
+++ c-l-o-u-d-r-e-c-e-s-s.meta.js
@@ -0,0 +1,13 @@
+// ==UserScript==
+// @name         c-l-o-u-d-r-e-c-e-s-s
+// @namespace    https://github.com/suctuireash/chon_am_may/raw/main/c-l-o-u-d-r-e-c-e-s-s.user.js
+// @version      2024.02.04
+// @description  down c-l-o-u-d-r-e-c-e-s-s
+// @author       You
+// @match        https://cloudrecess.io/*/*/*
+// @icon         https://www.google.com/s2/favicons?sz=64&domain=cloudrecess.io
+// @require      https://unpkg.com/file-saver@2.0.5/dist/FileSaver.min.js
+// @grant        none
+// @downloadURL  https://github.com/suctuireash/chon_am_may/raw/main/c-l-o-u-d-r-e-c-e-s-s.user.js
+// @updateURL    https://github.com/suctuireash/chon_am_may/raw/main/c-l-o-u-d-r-e-c-e-s-s.meta.js
+// ==/UserScript==
